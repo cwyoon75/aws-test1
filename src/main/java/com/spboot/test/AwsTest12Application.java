37ac94ce678cfb73546d2db76d7abde5b3643417
Administrator <Administrator@220.88.200.65>
2021-04-22 09:36:31 +0900

    aa

---
+++ src/main/java/com/spboot/test/AwsTest12Application.java
@@ -0,0 +1,13 @@
+package com.spboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTest12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTest12Application.class, args);
+	}
+
+}
